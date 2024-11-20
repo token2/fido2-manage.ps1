db7f8b4160205c9a3d41178adb60f8fc03293da6
Token2 <token2@users.noreply.github.com>
2024-11-20 15:27:25 +0100

    Update prepare.ps1
    Installation from PS Gallery

--- prepare.ps1
+++ prepare.ps1
@@ -1,14 +1,17 @@
+# Check if ps2exe is installed, if not, install it from PowerShell Gallery
+if (-not (Get-Command ps2exe -ErrorAction SilentlyContinue)) {
+    Write-Host "ps2exe is not installed. Installing from PowerShell Gallery..."
 
-
-# Check if ps2exe is installed, if not, install it
-if (-not (Get-Command ps2exe.exe -ErrorAction SilentlyContinue)) {
-    Write-Host "ps2exe is not installed. Installing..."
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/MScholtes/PS2EXE/master/Module/ps2exe.ps1'))
+    # Install ps2exe module from PowerShell Gallery
+    Install-Module -Name ps2exe -Force -Scope CurrentUser
 }
+
+# Import ps2exe module to ensure it's loaded
+Import-Module ps2exe
 
 # Compile fido2-manage.ps1 to fido2-manage.exe, console with admin rights requirement
 Write-Host "Compiling fido2-manage.ps1 to fido2-manage.exe..."
-ps2exe -inputFile fido2-manage.ps1 -outputFile fido2-manage.exe  -requireAdmin
+ps2exe -inputFile fido2-manage.ps1 -outputFile fido2-manage.exe -requireAdmin
 
 # Compile gui.ps1 to "_FIDO2.1 Manager.exe", no-console with admin rights requirement and icon of t2.ico (same folder)
 Write-Host "Compiling gui.ps1 to _FIDO2.1 Manager.exe..."
