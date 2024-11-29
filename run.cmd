a40c16b5282e10205af80ae54491674c23becf35
Token2 <token2@users.noreply.github.com>
2024-11-29 21:27:02 +0100

    Delete run.cmd

--- run.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Get the directory of the current .bat file
-set CurrentDir=%~dp0
-
-REM Change to that directory
-cd /d "%CurrentDir%"
-
-REM Run the PowerShell script
-powershell -ExecutionPolicy Bypass -File "%CurrentDir%gui.ps1"
