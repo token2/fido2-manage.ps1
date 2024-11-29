0abd476554775c27c2e5f7f72cb10e3548b383a2
Token2 <token2@users.noreply.github.com>
2024-11-29 21:33:35 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,5 +5,10 @@ set CurrentDir=%~dp0
 REM Change to that directory
 cd /d "%CurrentDir%"
 
+REM Rename the file if it exists
+if exist "FIDO2.1.Manager.ps1.txt" (
+    rename "FIDO2.1.Manager.ps1.txt" "FIDO2.1.Manager.ps1"
+)
+
 REM Run the PowerShell script
-powershell -ExecutionPolicy Bypass -File "%CurrentDir%FIDO2.1.Manager.ps1
+powershell -ExecutionPolicy Bypass -File "%CurrentDir%FIDO2.1.Manager.ps1"
